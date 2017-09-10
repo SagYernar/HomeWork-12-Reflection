a13c6f26bc8095dc99b0245fc33994e3f337ceb1
SagYernar <sagyernar@gmail.com>
2017-09-10 19:14:07 +0600

    ДЗ по рефлексии
    Оба задания в одном файле

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача №1\n");
+
+            Type typeForMethods = typeof(Console);
+
+            foreach (MethodInfo method in typeForMethods.GetMethods())
+            {
+                Console.WriteLine(method.ToString());
+            }
+
+            //Способ через GetType
+            //Type type1 = Type.GetType("System.Console");
+
+            //foreach (MethodInfo method in type1.GetMethods())
+            //{
+            //    Console.WriteLine(method.ToString());
+            //}
+
+            Console.WriteLine("\nНажмите Enter для демонстрации следующей задачи");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Задача №2\n");
+
+            Properties properties = new Properties();
+            Type typeForProperties = typeof(Properties);
+
+            foreach (PropertyInfo property in typeForProperties.GetProperties())
+            {
+                Console.WriteLine(property.ToString() + " - " + property.GetValue(properties));
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Properties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Properties
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Properties()
+        {
+            Name = "Properties";
+            Id = 1;
+        }
+    }
+}
